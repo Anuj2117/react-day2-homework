2abb2e6e5d75b2496593f7209c39900a8eb94289
Anuj2117 <anujkushwaha221@gmail.com>
2024-07-01 12:32:14 +0530

    create new repo for assignment

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="wrapper">
+        <h1>Topic Covered</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores ipsam, repudiandae numquam quidem ut recusandae!</p>
+        <a href="">Getting started with web development</a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis asperiores minus sit, reprehenderit quis repellat!</p>
+        <a href="">HTML-Structuring the web</a>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit voluptates, blanditiis delectus vero debitis atque?</p>
+        <a href="">CSS- Styling the web</a>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt voluptate veniam quae quo sit tenetur in debitis repellat aperiam minus.</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
